67e0c57bb09cfa9232cb59877d76ee30e9216da5
Sky.Allen <894982165@qq.com>
2024-04-09 10:13:49 +0800

    1

--- assets/resload/ResLoad.ts
+++ assets/resload/ResLoad.ts
@@ -1,8 +1,19 @@
-import { _decorator, Component,SpriteFrame, Node,assetManager,Sprite, resources } from 'cc';
+import { _decorator, Component,SpriteFrame, Node,Sprite,TextAsset,AudioClip, resources, RichText, JsonAsset, AudioSource } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('ResLoad')
 export class ResLoad extends Component {
+
+
+    @property(RichText)
+    rt:RichText|null=null;
+
+    @property(Sprite)
+    sp:Sprite|null=null;
+
+    @property(AudioSource)
+    aus:AudioSource|null=null;
+
     start() {
         // assetManager.resources.load("laser",(err,res)=>{
         //     if(err) return;
@@ -17,6 +28,56 @@ export class ResLoad extends Component {
             console.log("res",res);
         });
         */
+       
+        // this.loadText("h副本",(txt)=>{
+        //     this.rt.string=txt;
+        // } )
+
+
+        // this.loadJson("hh",(json)=>{
+        //     console.log(json.aa);
+        //     console.log(json.bb);
+        // })
+
+        this.loadSprite("red",(sp)=>{
+            this.sp.spriteFrame=sp;
+        });
+
+        // this.loadAudioClip("laser",(clip)=>{
+        //     this.aus.clip=clip;
+        //     this.aus.play();
+        // })
+    }
+
+    loadText(filePath,func){
+        resources.load(filePath, TextAsset, function (err, asset) {
+            if(err) return;
+            func(asset.text);
+        });
+    }
+
+    loadJson(filePath,func){
+        resources.load(filePath, JsonAsset, function (err, asset) {
+            if(err) return;
+            func( asset.json);
+        });
+    }
+
+    loadSprite(filePath,func){
+        resources.load(filePath, SpriteFrame, function (err, asset) {
+            if(err) {
+                console.log("load sp error");
+                return;
+            }
+            func(asset);
+        });
+    }
+
+    loadAudioClip(filePath,func){
+        resources.load(filePath, AudioClip, function (err, asset) {
+            if(err) return;
+            func( asset);
+        });
     }
 
     update(deltaTime: number) {
