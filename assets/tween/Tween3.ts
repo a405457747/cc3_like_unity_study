1029a1d2e3bc78cbe3feac5aba194ebaabfaf9b7
Sky.Allen <894982165@qq.com>
2024-05-30 10:23:44 +0800

    2

--- assets/tween/Tween3.ts
+++ assets/tween/Tween3.ts
@@ -1,23 +1,55 @@
 import { _decorator, Component, Node,Tween,tween, Vec3 } from 'cc';
+import { tween2 } from './tween2';
 const { ccclass, property } = _decorator;
 
-
+interface KK {
+  JJ();
+}
 
 @ccclass('Tween3')
 export class Tween3 extends Component {
 
     private t:Tween<Node>=null;
+
+    private t2:Tween<Node>=null;
+
+    private t3:tween2=null;
+
     start() {
+      
+      console.log("hello2:"+Infinity);
    this.t=   tween(this.node)
-      .to(1,{position:new Vec3(100,0,0)})
-      .to(1,{position:new Vec3(-100,0,0)})
-      .union()
-      .repeatForever()
+      .to(5,{position:new Vec3(100,0,0)});
+    this.t2=tween(this.node)
+      .to(5,{position:new Vec3(-100,0,0)});
+
+      this.t.then(this.t2)
       .start();
 
+     let k:string= this.add<string>("3");
+     let k2:number=this.add<number>(3);
+      /*
       setTimeout(() => {
         this.t.stop();
       }, 2000);
+      */
+    }
+
+    add<KK>(a:KK):KK{
+      return a;
+    }
+
+    JJ(){
+      return 3;
+    }
+
+    protected onDisable(): void {
+      this.t.stop();
+      console.log("dis");      
+    }
+
+    protected onEnable(): void {
+      console.log("ena");      
     }
 
     update(deltaTime: number) {
--- assets/tween/tween2.ts
+++ assets/tween/tween2.ts
@@ -3,6 +3,22 @@ const { ccclass, property } = _decorator;
 
 @ccclass('tween2')
 export class tween2 extends Component {
+
+   private _Age:number=3;
+   private _Name:string="hello";
+
+
+   private _addAge(){
+        this._Age+=1;
+    }
+
+   private _NameS(){
+        this._Name+="bill";
+    }
+    JJ(){
+        return 2;
+    }
+
     start() {
         console.log("hello");
         tween()
